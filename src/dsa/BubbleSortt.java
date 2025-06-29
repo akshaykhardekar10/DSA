2081594b05d40e8110bd1989833d71bd56a88c88
akshaykhardekar10 <khardekarakshay33@gmail.com>
2025-06-29 13:50:09 +0530

    sorting

--- src/dsa/BubbleSortt.java
+++ src/dsa/BubbleSortt.java
@@ -3,7 +3,7 @@ package dsa;
 public class BubbleSortt {
     public static void main(String[] args) {
         int count=0;
-        int []arr={64, 34, 25, 12, 22, 11, 90, 5};
+        int[] arr={64, 34, 25, 12, 22, 11, 90, 5};
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
@@ -11,22 +11,25 @@ public class BubbleSortt {
 
 
         for (int i = 0; i < arr.length-1; i++) {
-
+        boolean flag =false;
             for (int j = 0; j < arr.length-1-i; j++) {
 
                 if (arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
-
+                    flag= true;
                     count++;
 
+                }}
+                if (!flag){
+                    break;
                 }
 
-            }
+
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int j : arr) {
+            System.out.println(j);
         }
         System.out.println("count is "+count);
     }
--- src/dsa/RecrsionArrays.java
+++ src/dsa/RecrsionArrays.java
@@ -8,8 +8,6 @@ public class RecrsionArrays{
         System.out.print(arr[index] +" ");
         printArrayRecursive(arr, index + 1);
     }
-
-
     public static String removeChar(String str , int index,char wordToRemove){
         if (str.length()==index) return "";
 
@@ -22,12 +20,25 @@ public class RecrsionArrays{
             return smallAns;
         }
     }
+    static void printSubSeq(String s , String currAns){
+        if(s.length()==0){
+            System.out.println(currAns);
+            return;
+        }
+
+        char currChar = s.charAt(0);
+        String remString = s.substring(1);
+        printSubSeq(remString , currAns+currChar);
+        printSubSeq(remString, currAns);
+    }
+    
 
     public static void main(String[] args) {
     RecursionPractice recursionPractice = new RecursionPractice();
   
      //   printArrayRecursive(new int[] {1,2,2,3,4,45,6,67,7,88,2}, 0);
-        System.out.println(removeChar("akshay khardekar" , 0, 'h'));
+//        System.out.println(removeChar("akshay khardekar" , 0, 'h'));
+    printSubSeq("abc" , "");
     }
 }
 
